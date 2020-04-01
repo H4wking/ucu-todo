cb4c8430b8fae50b39631dbb187ce1634b8925d8
H4wking <44340939+H4wking@users.noreply.github.com>
2020-04-02 01:02:03 +0300

    Add files via upload

---
+++ src/lib/stepan.js
@@ -0,0 +1,54 @@
+
+export default class Stepan {
+  static createElement(element, parent, attributes = {}) {
+    // TODO: check if this is a valid tag name
+    const newElement = document.createElement(element);
+
+    if (newElement instanceof HTMLUnknownElement) {
+      throw new StepanError("Invalid tag name");
+    }
+
+    const { innerHTML, innerText } = attributes;
+
+    for (let attribute in attributes) {
+      if (['innerHTML', 'innerText'].includes(attribute)) {
+        continue;
+      }
+
+      newElement.setAttribute(attribute, attributes[attribute]);
+    }
+
+    innerHTML && (newElement.innerHTML = innerHTML);
+    innerText && (newElement.innerText = innerText);
+
+    parent.appendChild(newElement);
+
+    return newElement;
+  }
+
+  static Component = class {
+    constructor(parent) {
+
+      // TODO: 1. Create StepanError class to define all framework errors
+      //       2. throw an error if parent is null or undefined, or if it's not a valid DOM object
+      if (parent == null || parent == undefined) {
+        throw new StepanError("Parent is null or undefined");
+      }
+
+      if (!(parent.nodeType === 1)) {
+        throw new StepanError("Parent is not a valid DOM object");
+      }
+
+      this.parent = parent;
+    }
+
+    // TODO (Bonus): Ensure that every component returns a top-level root element
+  }
+}
+
+class StepanError extends Error {
+  constructor(message) {
+  super(message);
+  this.name = "StepanError";
+  }
+}
